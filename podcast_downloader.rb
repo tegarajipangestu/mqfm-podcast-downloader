3ea19f7801ee620c23ea8990cf405102bdd3876b
tegarajipangestu <13512061@std.stei.itb.ac.id>
2016-05-12 10:29:46 +0700

    Adding directory

--- podcast_downloader.rb
+++ podcast_downloader.rb
@@ -1,15 +1,18 @@
 require 'open-uri'
 require 'json'
 require 'progressbar'
+require 'fileutils'
 
-date = '2016_05_11'
+date = '2016_05_12'
 url = 'http://streaming.rajawaliradio.com/mqod/jsonp.php?date='+date+'&callback=&format=MP3'
 pbar = nil
 
 json = JSON.parse(open(url).read.tr('()',''))
 json.each do |j|
   filename = j["acara"]+".mp3" 
-  File.open(j["acara"]+".mp3", "wb") do |saved_file|
+  path = File.expand_path "download/"+date+"/"
+  FileUtils.mkdir_p path
+  File.open("download/"+date+"/"+j["acara"]+".mp3", "wb") do |saved_file|
     open(URI.escape(j["link"]).gsub('[','%5B').gsub(']','%5D'), "rb", :content_length_proc => lambda { |t|
     if t && 0 < t
       pbar = ProgressBar.new(j["acara"], t)
